a037ab8e7864ad2b9e81fcec78211a4eb8f4feb6
fpaniaguajava <fernando.paniagua.java@gmail.com>
2026-03-11 12:08:08 +0100

    Initial commit

---
+++ src/main/java/com/cursospring/IFCD0014_2026_02_HolaMundoSpringWSJPA/Ifcd0014202602HolaMundoSpringWsjpaApplication.java
@@ -0,0 +1,13 @@
+package com.cursospring.IFCD0014_2026_02_HolaMundoSpringWSJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ifcd0014202602HolaMundoSpringWsjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ifcd0014202602HolaMundoSpringWsjpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursospring/IFCD0014_2026_02_HolaMundoSpringWSJPA/Ifcd0014202602HolaMundoSpringWsjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursospring.IFCD0014_2026_02_HolaMundoSpringWSJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ifcd0014202602HolaMundoSpringWsjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
